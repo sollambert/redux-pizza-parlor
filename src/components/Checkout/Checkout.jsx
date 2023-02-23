f688f2ee2122b8b612eace107c4c76a7838d59f6
Xai Hang <xaihang12@gmail.com>
2023-02-23 14:13:53 -0600

    added styles to checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,12 +1,15 @@
-import axios from "axios";
-import { useState } from "react";
-import { useSelector } from "react-redux";
+import axios from 'axios';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import './Checkout.css';
 
 function Checkout() {
-    // const order = useSelector('order')
+  // const order = useSelector('order')
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-
-   const [order, setOrders ] = useState({
+  const [order, setOrders] = useState({
     customer_name: 'Xai',
     street_address: '1111 lee ave',
     city: 'Brooklyn Center',
@@ -14,82 +17,78 @@ function Checkout() {
     type: 'Pick up',
     total: 22.22,
     pizzas: [
-        {
-            name:  'Tomato Soup',
-            description: 'If you like pizza, but you hate the toppings, the cheese, and the crust, you\'ll love this!',
-            price: 12.99,
-            image_path: 'images/pizza_photo.png'
-        },
-        {
-            name:  'Tomato Soup',
-            description: 'If you like pizza, but you hate the toppings, the cheese, and the crust, you\'ll love this!',
-            price: 12.99,
-            image_path: 'images/pizza_photo.png'
-        }
-    ]
-   })
+      {
+        name: 'Tomato Soup',
+        description:
+          "If you like pizza, but you hate the toppings, the cheese, and the crust, you'll love this!",
+        price: 12.99,
+        image_path: 'images/pizza_photo.png',
+      },
+      {
+        name: 'Tomato Soup',
+        description:
+          "If you like pizza, but you hate the toppings, the cheese, and the crust, you'll love this!",
+        price: 12.99,
+        image_path: 'images/pizza_photo.png',
+      },
+    ],
+  });
 
-   const handleCheckout = () => {
+  //! when checkout btn is clicked
+  const handleCheckout = () => {
     //axios call here
-    console.log('here', order)
-    axios.post('/api/order/', order)
-    .then((response) => {
-      // console.log('success - product added to database')
-      dispatch({ type: 'CLEAR_CART' });
+    console.log('here', order);
+    axios
+      .post('/api/order/', order)
+      .then((response) => {
+        // console.log('success - pizzas added to database')
+        dispatch({ type: 'CLEAR_CART' });
 
-      //Navigate back to pizza list
-      history.push('/');
-    })
-    .catch((error)=> {
-      console.log('error in adding product to database')
-    })
-   }
-
+        //! Navigate back to pizza list
+        history.push('/');
+      })
+      .catch((error) => {
+        console.log('error in adding product to database');
+      });
+  };
 
   return (
     <div>
-      <h1>Step 3: Checkout</h1>
-      <p>{order.customer_name}</p>
-      <p>{order.street_address}</p>
-      <p>{order.city}</p>
-      <p>{order.zip}</p>
+      <h2>Step 3: Checkout</h2>
+      <div className="order-type">
       <p>{order.type} (delivery or pickup)</p>
-
-  
+      </div>
+      <div className="customer-info">
+        <p>{order.customer_name}</p>
+        <p>{order.street_address}</p>
+        <p>{order.city}</p>
+        <p>{order.zip}</p>
+      </div>
+     
       <table>
         <thead>
-  <tr>
-    <th>Name</th>
-    <th>Cost</th>
-  </tr>
-  </thead>
-    <tbody>
-  {order.pizzas.map((pizza, i) => {
-    return (
-        <tr key ={i}>
-        <td>{pizza.name}</td>
-        <td>{pizza.price}</td>
-      </tr>
-    )
-  })}
-  </tbody>
-</table>
+          <tr>
+            <th>Name</th>
+            <th>Cost</th>
+          </tr>
+        </thead>
+        <tbody>
+          {order.pizzas.map((pizza, i) => {
+            return (
+              <tr key={i}>
+                <td>{pizza.name}</td>
+                <td>{pizza.price}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
 
+      <h3>Total: {order.total}</h3>
 
-<h2>Total: {order.total}</h2>
-
-<button onClick={handleCheckout}>Checkout</button>
-
+      <button onClick={handleCheckout}>CHECKOUT</button>
     </div>
   );
 }
 
 export default Checkout;
-
-// "customer_name" VARCHAR (1000) NOT NULL,
-// "street_address" VARCHAR(1000) NOT NULL,
-// "city" VARCHAR(1000) NOT NULL,
-// "zip" VARCHAR(20) NOT NULL,
-// "type" VARCHAR(100) NOT NULL,
-// "total" NUMERIC (20, 2) NOT NULL,
-// "time
