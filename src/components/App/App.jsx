82615601a7641e431a28cd2e0474476397e1d13f
Xai Hang <xaihang12@gmail.com>
2023-02-23 14:19:00 -0600

    resolve conflicts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import Checkout from '../Checkout/Checkout';
-=======
+
 import {useSelector, useDispatch} from 'react-redux';
 import {useEffect} from 'react';
 import axios from 'axios';
 import { HashRouter as Router, Route, Link } from 'react-router-dom';
 
 import PizzaList from '../PizzaList/PizzaList';
->>>>>>> origin
+
 
 function App() {
 
@@ -29,19 +28,6 @@ function App() {
   }
 
   return (
-<<<<<<< HEAD
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-     
-      <img src='images/pizza_photo.png' />
-      <p>Pizza is great.</p>
-      <Checkout />
-  
-    </div>
-=======
-    <>
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Prime Pizza</h1>
@@ -55,19 +41,16 @@ function App() {
                 {/* CustomerForm component */}
               </Route>
               <Route exact path="/checkout">
-                {/* Checkout component */}
+                <Checkout />
               </Route>
               <Route exact path="/admin">
                 {/* Admin component */}
               </Route>
             </div>
           </Router>
-
         </header>
       </div>
-    </>
 
->>>>>>> origin
   );
 }
 
