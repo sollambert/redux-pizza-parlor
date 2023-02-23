3baa49fa4fec326c2e62fefbc05ec8eea7019495
Anniessa Antar <anniessa.antar@gmail.com>
2023-02-23 13:34:23 -0600

    create routes for different components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+
+import PizzaList from '../PizzaList/PizzaList';
 
 function App() {
 
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Prime Pizza</h1>
-      </header>
-  
+    <>
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>Prime Pizza</h1>
+
+          <Router>
+            <div>
+              <Route exact path="/">
+                {/* <PizzaList /> */}
+              </Route>
+              <Route exact path="/customerForm">
+                {/* CustomerForm component */}
+              </Route>
+              <Route exact path="/checkout">
+                {/* Checkout component */}
+              </Route>
+              <Route exact path="/admin">
+                {/* Admin component */}
+              </Route>
+            </div>
+          </Router>
+
+        </header>
+      </div>
+
+
+
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
-    </div>
+    </>
+
   );
 }
 
