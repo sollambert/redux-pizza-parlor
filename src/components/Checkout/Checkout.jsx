d1097dace9cd106c4d8cd079425779f6693fe4a6
Ben Capp <bentucker333@gmail.com>
2023-02-23 16:02:07 -0600

    refactored header and added path on form

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,8 +1,10 @@
-import axios from 'axios';
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import './Checkout.css';
+import axios from "axios";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import Header from "../Header/Header";
+import "./Checkout.css";
 
 function Checkout() {
   // const order = useSelector('order')
@@ -10,26 +12,26 @@ function Checkout() {
   const history = useHistory();
 
   const [order, setOrders] = useState({
-    customer_name: 'Xai',
-    street_address: '1111 lee ave',
-    city: 'Brooklyn Center',
+    customer_name: "Xai",
+    street_address: "1111 lee ave",
+    city: "Brooklyn Center",
     zip: 22222,
-    type: 'Pick up',
+    type: "Pick up",
     total: 22.22,
     pizzas: [
       {
-        name: 'Tomato Soup',
+        name: "Tomato Soup",
         description:
           "If you like pizza, but you hate the toppings, the cheese, and the crust, you'll love this!",
         price: 12.99,
-        image_path: 'images/pizza_photo.png',
+        image_path: "images/pizza_photo.png",
       },
       {
-        name: 'Tomato Soup',
+        name: "Tomato Soup",
         description:
           "If you like pizza, but you hate the toppings, the cheese, and the crust, you'll love this!",
         price: 12.99,
-        image_path: 'images/pizza_photo.png',
+        image_path: "images/pizza_photo.png",
       },
     ],
   });
@@ -37,57 +39,60 @@ function Checkout() {
   //! when checkout btn is clicked
   const handleCheckout = () => {
     //axios call here
-    console.log('here', order);
+    console.log("here", order);
     axios
-      .post('/api/order/', order)
+      .post("/api/order/", order)
       .then((response) => {
         // console.log('success - pizzas added to database')
-        dispatch({ type: 'CLEAR_CART' });
+        dispatch({ type: "CLEAR_CART" });
 
         //! Navigate back to pizza list
-        history.push('/');
+        history.push("/");
       })
       .catch((error) => {
-        console.log('error in adding product to database');
+        console.log("error in adding product to database");
       });
   };
 
   return (
-    <div>
-      <h2>Step 3: Checkout</h2>
-      <div className="order-type">
-      <p>{order.type} (delivery or pickup)</p>
-      </div>
-      <div className="customer-info">
-        <p>{order.customer_name}</p>
-        <p>{order.street_address}</p>
-        <p>{order.city}</p>
-        <p>{order.zip}</p>
-      </div>
-     
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Cost</th>
-          </tr>
-        </thead>
-        <tbody>
-          {order.pizzas.map((pizza, i) => {
-            return (
-              <tr key={i}>
-                <td>{pizza.name}</td>
-                <td>{pizza.price}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+    <>
+      <Header />
+      <div>
+        <h2>Step 3: Checkout</h2>
+        <div className="order-type">
+          <p>{order.type} (delivery or pickup)</p>
+        </div>
+        <div className="customer-info">
+          <p>{order.customer_name}</p>
+          <p>{order.street_address}</p>
+          <p>{order.city}</p>
+          <p>{order.zip}</p>
+        </div>
 
-      <h3>Total: {order.total}</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            {order.pizzas.map((pizza, i) => {
+              return (
+                <tr key={i}>
+                  <td>{pizza.name}</td>
+                  <td>{pizza.price}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
 
-      <button onClick={handleCheckout}>CHECKOUT</button>
-    </div>
+        <h3>Total: {order.total}</h3>
+
+        <button onClick={handleCheckout}>CHECKOUT</button>
+      </div>
+    </>
   );
 }
 
--- src/components/CustomerInfoForm/CustomerInfoForm.jsx
+++ src/components/CustomerInfoForm/CustomerInfoForm.jsx
@@ -3,8 +3,20 @@ import "./CustomerInfoForm.css";
 import { useDispatch } from "react-redux";
 import { useState } from "react";
 
+import { useHistory } from "react-router-dom";
+
+import Header from "../Header/Header";
+
 function CustomerInfoForm() {
   const dispatch = useDispatch();
+
+  // history
+  const history = useHistory();
+
+  const goToCheckout = () => {
+    alert("Thanks for your order! Finalize your order on the next page.");
+    history.push("/checkout");
+  };
 
   // customer info object
   const [customerInfo, setCustomerInfo] = useState("");
@@ -25,6 +37,7 @@ function CustomerInfoForm() {
     ) {
       console.log("submitting customer, customer is:", customerInfo);
       dispatch({ type: "SET_CUSTOMER", payload: customerInfo });
+      goToCheckout();
     } else {
       setIncompleteMessage(true);
       setTimeout(() => {
@@ -69,63 +82,66 @@ function CustomerInfoForm() {
   };
 
   return (
-    <form id="customer-form" onSubmit={handleSubmit}>
-      <h3>Step 2: Customer Information</h3>
-      <div id="input-container">
-        <label htmlFor="name-input">Name: </label>
-        <input
-          onChange={handleNameChange}
-          name="name-input"
-          placeholder="your name here"
-        ></input>
-        <label htmlFor="address-input">Street Address: </label>
-        <input
-          onChange={handleStreetAddressChange}
-          name="address-input"
-          placeholder="your address here"
-        ></input>
-        <label htmlFor="city-input">City: </label>
-        <input
-          onChange={handleCityChange}
-          name="city-input"
-          placeholder="your city here"
-        ></input>
-        <label htmlFor="zip-input">Zip Code: </label>
-        <input
-          onChange={handleZipChange}
-          name="zip-input"
-          placeholder="your zip code here"
-        ></input>
-      </div>
-      <div id="radio-container">
-        <div id="pickup-container">
+    <>
+      <Header displayTotal={true} />
+      <form id="customer-form" onSubmit={handleSubmit}>
+        <h3>Step 2: Customer Information</h3>
+        <div id="input-container">
+          <label htmlFor="name-input">Name: </label>
           <input
-            onChange={handleTypeChange}
-            type="radio"
-            id="pickup"
-            name="fav_language"
-            value="pickup"
-          />
-          <label htmlFor="pickup">Pickup</label>
-        </div>
-        <div id="delivery-container">
+            onChange={handleNameChange}
+            name="name-input"
+            placeholder="your name here"
+          ></input>
+          <label htmlFor="address-input">Street Address: </label>
           <input
-            onChange={handleTypeChange}
-            type="radio"
-            id="delivery"
-            name="fav_language"
-            value="delivery"
-          />
-          <label htmlFor="delivery">Delivery</label>
+            onChange={handleStreetAddressChange}
+            name="address-input"
+            placeholder="your address here"
+          ></input>
+          <label htmlFor="city-input">City: </label>
+          <input
+            onChange={handleCityChange}
+            name="city-input"
+            placeholder="your city here"
+          ></input>
+          <label htmlFor="zip-input">Zip Code: </label>
+          <input
+            onChange={handleZipChange}
+            name="zip-input"
+            placeholder="your zip code here"
+          ></input>
         </div>
-      </div>
-      <button id="next-btn" type="submit">
-        NEXT
-      </button>
-      {displayIncompleteMessage && (
-        <p>Oops! Fill out all fields to go to the next page.</p>
-      )}
-    </form>
+        <div id="radio-container">
+          <div id="pickup-container">
+            <input
+              onChange={handleTypeChange}
+              type="radio"
+              id="pickup"
+              name="fav_language"
+              value="pickup"
+            />
+            <label htmlFor="pickup">Pickup</label>
+          </div>
+          <div id="delivery-container">
+            <input
+              onChange={handleTypeChange}
+              type="radio"
+              id="delivery"
+              name="fav_language"
+              value="delivery"
+            />
+            <label htmlFor="delivery">Delivery</label>
+          </div>
+        </div>
+        <button id="next-btn" type="submit">
+          NEXT
+        </button>
+        {displayIncompleteMessage && (
+          <p>Oops! Fill out all fields to go to the next page.</p>
+        )}
+      </form>
+    </>
   );
 }
 
